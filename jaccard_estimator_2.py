609a2336f5b3f6225cefc0df64edcd2c2129d467
nishatbristy007 <38611728+nishatbristy007@users.noreply.github.com>
2021-02-24 20:34:16 +0600

    Updated encoding functions.

--- jaccard_estimator_2.py
+++ jaccard_estimator_2.py
@@ -9,7 +9,6 @@ from subprocess import call, check_output, STDOUT
 import shutil
 import re
 import sys
-import multiprocessing as mp
 kmerdir="kmer_dir"
 encode_dir = "encodedfiles"
 encode_dir_replaced = "encodedfiles_replaced"
@@ -20,7 +19,12 @@ base_subs = 13
 base_str=['A','C','G','T']
 encode_str = ['11','10','01','00']
 tmp_name = "tmp"  
-def encodeKmer(kmer):
+
+first_bit_trans = str.maketrans('ACGT', '0110')
+second_bit_trans = str.maketrans('ACGT', '1010')
+
+
+def encodeKmer2(kmer):
     kmer = re.sub(r'A','11',kmer)
     kmer = re.sub(r'C','10',kmer)
     kmer = re.sub(r'G','01',kmer)
@@ -28,151 +32,261 @@ def encodeKmer(kmer):
     return np.int64(int(kmer,2))
 
 def replaceEncodedKmer(kmer,regex_,encode_base,k_fmt):
-    return np.int64(int(regex_.sub( encode_base, format(kmer,k_fmt)),2))
+    return np.int64(int(re.sub(regex_, encode_base, format(kmer,k_fmt)),2))
 
-def replace_encoding(kmer,first_base,second_base,ksize):
-    mask=int('1'*ksize,2)
-    b_str="{0:b}".format(kmer)
-    b_str=b_str.zfill(ksize*2)
-    b_first,b_second=b_str[::2],b_str[1::2]
-    x1=int(b_first,2)
-    x2=int(b_second,2)
-    if first_base=='A' and second_base=='C':
-        xnew=x1 ^ x2
-        x2=x2 & xnew
-    elif first_base=='C' and second_base=='A':
-        xnew=(x1 ^ x2)
-        x2=x2 | xnew
-    elif first_base=='A' and second_base=='G':
-        xnew=x1 ^ x2
-        x1=x1 & xnew
-    elif first_base=='G' and second_base=='A':
-        xnew=(x1 ^ x2)
-        x1=x1 | xnew
-    elif first_base=='A' and second_base=='T':
-        xnew=(x1 ^ x2)
-        x1=x1 & xnew
-        x2=x2 & xnew
-    elif first_base=='T' and second_base=='A':
-        xnew=(x1 | x2) ^ mask
-        x1=x1 ^ xnew
-        x2=x2 ^ xnew
-    elif first_base=='C' and second_base=='G':
-        xnew=x2 ^ mask
-        xnew=x1 & xnew
-        x1=x1 ^ xnew
-        x2=x2 ^ xnew
-    elif first_base=='G' and second_base=='C':
-        xnew=x1 ^ mask
-        xnew=x2 & xnew
-        x1=x1 ^ xnew
-        x2=x2 ^ xnew
-    elif first_base=='C' and second_base=='T':
-        xnew=x2 ^ mask
-        xnew=x1 & xnew
-        x1=x1 ^ xnew
-    elif first_base=='T' and second_base=='C':
-        xnew=(x1 | x2) ^ mask
-        x1=x1 ^ xnew
-    elif first_base=='G' and second_base=='T':
-        xnew=x1 ^ mask
-        xnew=x2 & xnew
-        x2=x2 ^ xnew
-    elif first_base=='T' and second_base=='G':
-        xnew=(x1 | x2) ^ mask
-        x2=x2 ^ xnew    
-    x1="{0:b}".format(x1)
-    x2="{0:b}".format(x2)
-    x1=x1.zfill(ksize)
-    x2=x2.zfill(ksize)
-    originalstring = ''.join([''.join(x) for x in zip(x1,x2)])
-     
+def encodeKmer(kmer):
+    global first_bit_trans, second_bit_trans
+    return np.int64(int(kmer.translate(first_bit_trans)+kmer.translate(second_bit_trans), 2))
+
+def A_to_C(x,size):
+    mask=int(('1'*size),2)
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+    xnew=x1 ^ mask
+    xnew=x2 & xnew 
+    x1=x1 ^ xnew
+    x2=x2 ^ xnew
+    return np.int64((x1 << size) + x2)
+
+def C_to_A(x,size):
+    mask=int(('1'*size),2)
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+    xnew=x2 ^ mask
+    xnew=x1 & xnew 
+    x1=x1 ^ xnew
+    x2=x2 ^ xnew
+    return np.int64((x1 << size) + x2)
+
+def A_to_G(x,size):
+    mask=int(('1'*size),2)
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+    xnew=x1 ^ mask
+    xnew=x2 & xnew 
+    x1=x1 ^ xnew
+    return np.int64((x1 << size) + x2)
+
+def G_to_A(x,size):
+    mask=int(('1'*size),2)
+    print("{0:b}".format(mask))
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
     
-    return np.int64(int(originalstring,2))
-    
+    x1 = x1 & (x1 ^ x2)
+    return np.int64((x1 << size) + x2)
+	
+def A_to_T(x,size):
+    mask=int(('1'*size),2)
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+    xnew=x1 ^ mask
+    xnew=x2 & xnew 
+    x2=x2 ^ xnew
+    return np.int64((x1 << size) + x2)
+
+def T_to_A(x,size):
+    mask=int(('1'*size),2)
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+    xnew=(x1 | x2) ^ mask
+    x2=x2 ^ xnew
+    return np.int64((x1 << size) + x2)
+
+def C_to_G(x,size):
+    mask=int(('1'*size),2)
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+    xnew=x2 ^ mask
+    xnew=x1 & xnew 
+    x2=x2 ^ xnew
+    return np.int64((x1 << size) + x2)
+
+def G_to_C(x,size):
+    mask=int(('1'*size),2)
+    print("{0:b}".format(mask))
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+
+    x2 = x2 & (x1 ^ x2)
+    return np.int64((x1 << size) + x2)
+
+def C_to_T(x,size):
+    mask=int(('1'*size),2)
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+    xnew=x2 ^ mask
+    xnew=x1 & xnew 
+    x1=x1 ^ xnew
+    return np.int64((x1 << size) + x2)
+
+def T_to_C(x,size):
+    mask=int(('1'*size),2)
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+    xnew=(x1 | x2) ^ mask
+    x1=x1 ^ xnew
+    return np.int64((x1 << size) + x2)
+	
+def G_to_T(x,size):
+    mask=int(('1'*size),2)
+    print("{0:b}".format(mask))
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+    xnew=x1 ^ x2
+    x1= x1 & xnew
+    x2 = x2 & xnew
+    return np.int64((x1 << size) + x2)
+
+def T_to_G(x,size):
+    mask=int(('1'*size),2)
+    x1= (x & (mask<<size)) >> size
+    x2 = x & mask
+    xnew=(x1 | x2) ^ mask
+    x1=x1 ^ xnew
+    x2=x2 ^ xnew
+    return np.int64((x1 << size) + x2)
+
 def saveEncoding(k):
-    global tmp_name
     folderpath="kmer_dir"
     files = os.listdir(folderpath)
+    print(files)
     for file in files:
-        taxaname=file.split(".")[0]
-        sys.stderr.write('File read for {0}...\n'.format(file))
-        
-        kmers = open(folderpath+"/"+taxaname+".txt", 'r')
         sys.stderr.write('Encoding starting for {0}...\n'.format(file))
+        taxaname=file.split(".")[0]
+
+        kmers = open(folderpath+"/"+taxaname+".txt", 'r')
         start=time.time()
-        set1 = np.array([encodeKmer(kmer.split()[0]) for kmer in kmers], dtype =  np.int64)
-        new_dict={tmp_name:set1}
+        #set1 = set([encodeKmer(kmer.split()[0]) for kmer in kmers])
+        set1 = np.array([encodeKmer(kmer.split()[0]) for kmer in kmers], dtype = np.int64)
+        sys.stderr.write('Time taken for encoding {0}.\n'.format(time.time()-start))
+
+        start = time.time()
+        
         with open("encodedfiles/"+taxaname+'.pickle', 'wb') as f:
-            pickle.dump(new_dict,f)
+            pickle.dump({tmp_name:set1},f)
         end=time.time()
 
         sys.stderr.write('Encoding done for {0}.\n'.format(file))
-        sys.stderr.write('Time taken {0}.\n'.format(end-start))
+        #sys.stderr.write('Time taken for saving pickle file {0}.\n'.format(end-start))
 
         # Deleteing the kmer file from kmer_dir
         call(["rm",folderpath+"/"+file],stderr=open(os.devnull, 'w'))
+		
+def selectFunction(i,j):
+    if i == 0 and j == 1:
+        func = A_to_C
+    elif i == 0  and j == 2:
+        func = A_to_G
+    elif i ==0 and j == 3:
+        func = A_to_T
+    elif i == 1 and j == 0:
+        func = C_to_A
+    elif i == 1 and j == 2:
+        func = C_to_G
+    elif i == 1 and k == 3:
+        func = C_to_T
+    elif i == 2 and j == 0:
+        func = G_to_A
+    elif i == 2 and j == 1:
+        func = G_to_C
+    elif i == 2 and k == 3:
+        func = G_to_T
+    elif i == 3 and j == 0:
+        func = T_to_A
+    elif i == 3 and j == 1:
+        func = T_to_C
+    elif i == 3 and k == 2:
+        func = T_to_G
+    return func
     
+def sortintersection(x,y):
+    i=0
+    j=0
+    lenx=len(x)
+    leny=len(y)
+    while i < lenx and j < leny:
+        if x[i] == y[j]:
+            yield x[i]
+            i += 1
+            j += 1
+        elif x[i] > y[j]:
+            j += 1
+        else:
+            i += 1
+            
 def saveReplacedEncoding(k, i, j):
-    global encode_dir, encode_dir_replaced, tmp_name, base_str
+    global encode_dir, encode_dir_replaced
     if os.path.exists(encode_dir_replaced):
         shutil.rmtree(encode_dir_replaced)
     os.mkdir(encode_dir_replaced)
-
+	
+	
+    func = selectFunction(i,j)		
     files = os.listdir(encode_dir)
     for file in files:
         taxaname=file.split(".")[0]
-        sys.stderr.write("opening pickle file\n")
         start = time.time()
         with open("encodedfiles/"+taxaname+".pickle",'rb') as f:
             set1 = pickle.load(f)[tmp_name]
-        end = time.time()
-        sys.stderr.write('Time taken for opening {0}.\n'.format(end-start))
-
+        #sys.stderr.write('Time for loading pickle file {0}...\n'.format(time.time()-start))
         sys.stderr.write('Encoding starting for {0}...\n'.format(file+base_str[i]+base_str[j]))
         start=time.time()
-        regex_ = re.compile(encode_str[i]+'(?=(?:[\d0-1]{2})*$)')
+        '''
+		regex_ = encode_str[i]+'(?=(?:[\d0-1]{2})*$)'
         encode_base = encode_str[j]
         k_fmt = '0'+str(2*k)+'b'
-        from_str = base_str[i]
-        to_str = base_str[j]
-        #set2=np.array([np.int64(int(re.sub(regex_, encode_base, format(kmer,k_fmt)),2)) for kmer in set1])
-        set2 = np.array([replace_encoding(kmer, from_str, to_str, k) for kmer in set1])
-        #set2 = np.array([replaceEncodedKmer(kmer,regex_, encode_base,k_fmt) for kmer in set1])
+        set2=np.array([np.int64(int(re.sub(regex_, encode_base, format(kmer,k_fmt)),2)) for kmer in set1])
+        '''
+		#set2=np.array([np.int64(int(re.sub(regex_, encode_base, format(kmer,k_fmt)),2)) for kmer in set1])
+        set2=np.array([func(kmer, k) for kmer in set1],dtype=np.int64)
+        #set2=set([func(kmer, k) for kmer in set1])
         
-        #pool_sketch = mp.Pool(mp.cpu_count() -1)
-        #results_sketch = [pool_sketch.apply_async(replaceEncodedKmer, args=(kmer,regex_, encode_base,k_fmt)) for kmer in set1]
-        sys.stderr.write("Encoding done\n")
-        #set2 = np.array([result.get(9999999) for result in results_sketch], dtype=np.int64)#(len(pathnames))]
-        #for result in results_sketch:
-        #    result.get(9999999)
-        #pool_sketch.close()
-        #pool_sketch.join()
-        
-        
-        new_dict={tmp_name: set2}
+        sys.stderr.write('Time taken for encoding {0}.\n'.format(time.time()-start))
+
+        start = time.time()
         with open("encodedfiles_replaced/"+taxaname+base_str[i]+base_str[j]+'.pickle', 'wb') as f:
-            pickle.dump(new_dict,f)
-        end=time.time()
+            pickle.dump({tmp_name: set2},f)
         sys.stderr.write('Encoding saved for {0}...\n'.format(file+base_str[i]+base_str[j]))
-        sys.stderr.write('Time taken {0}.\n'.format(end-start))
+        sys.stderr.write('Time taken for saving pickle {0}.\n'.format(time.time()-start))
 
 def estimateJaccard(folderpath, n_taxa):
-    global tmp_name
+    sys.stderr.write("estimateJaccard\n") 
     files = os.listdir(folderpath)
-    jaccard_matrix = np.zeros((n_taxa,n_taxa))
+    jaccard_matrix = np.zeros(( n_taxa,n_taxa))
     for  i in range(n_taxa-1):
         for j in range(i+1, n_taxa):
+            #print(files[i], files[j])
+            start = time.time()
             with open(folderpath+'/'+files[i],'rb') as f:
                 set1 = pickle.load(f)[tmp_name]
             with open(folderpath+'/'+files[j],'rb') as f:
                 set2 = pickle.load(f)[tmp_name]
+            #sys.stderr.write('Time taken for reading pickles {0}.\n'.format(time.time()-start))
+            
+            start = time.time()
+            #set1 = set(set1)
+            #set2 = set(set2)
+            #sys.stderr.write('Time taken for making sets {0}.\n'.format(time.time()-start))
+            #start = time.time()
+            #xsorted = np.sort(set1)#sorted(set1)
+            #ysorted = np.sort(set2)#sorted(set2)
+            #sys.stderr.write('Time taken for sorting list {0}.\n'.format(time.time()-start))
             start = time.time()
             intersec = np.intersect1d(set1,set2)
             intersec_len=len(intersec)
-            union_len = len(np.union1d(set1,set2))
-            end = time.time()
-            sys.stderr.write('Time taken for intersection and union {0}.\n'.format(end-start))
+            #intersec_len = len(list(sortintersection(xsorted,ysorted)))
+            sys.stderr.write('Time taken for intersection {0}.\n'.format(time.time()-start))
+            #start = time.time()
+            union_len = len(set1)+len(set2) - intersec_len
+            #sys.stderr.write('Time taken for len union {0}.\n'.format(time.time()-start))
+            #intersec = np.intersect1d(set1,set2)
+            #intersec_len=len(intersec)
+            #start = time.time()
+            #union_len = len(np.union1d(set1,set2))
+            #end = time.time()
+            #sys.stderr.write('Time taken for union1d union {0}.\n'.format(time.time()-start))
+
             jaccard=intersec_len/union_len
             jaccard_matrix[i][j] = jaccard_matrix[j][i] = jaccard
     return jaccard_matrix
@@ -185,11 +299,11 @@ def distEstimatorMaster(k, n_taxa):
     # -- Creating directories to save encodings--
     if os.path.exists(encode_dir):
         shutil.rmtree(encode_dir)
-    
+    sys.stderr.write("n = {0}\n".format(n_taxa))
     os.mkdir(encode_dir)
     # --   --
 
-    # -- Distance (n,n) of the actual encodings --
+    # -- Jaccard (n,n) of the actual encodings --
     dist_matrices = np.zeros((base_subs,n_taxa,n_taxa))
     saveEncoding(k)
     dist_matrices[0] = estimateDistance(estimateJaccard(encode_dir,n_taxa),k)
